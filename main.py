d708320745fe7524cefc93e7f61ae53bea98e400
Abasiomofon Udoh <abasiomofonudoh@gmail.com>
2025-01-31 13:00:18 +0100

    Update main.py
    added timespec="seconds"

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    utc_time = datetime.datetime.now(pytz.UTC).isoformat()
+    utc_time = datetime.datetime.now(pytz.UTC).isoformat(timespec="seconds") + "Z"
     return {
         "email": "abasiomofonudoh@gmail.com",
         "current_datetime": utc_time,
